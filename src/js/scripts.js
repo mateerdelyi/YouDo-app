15a77c81d366de23fd163873e2ad073fb03e5974
Mate Erdelyi <mate.erdelyi@icloud.com>
2021-02-06 10:04:11 +0100

    MZ-02

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,8 @@
-//can be deleted
-console.log("Console log works!");
+function newTask() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("newTask").value;
+    var task = document.createTextNode(inputValue);
+    li.appendChild(task);
+    document.getElementById("tasksList").appendChild(li);
+}
+
