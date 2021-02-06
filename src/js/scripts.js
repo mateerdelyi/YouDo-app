e6323b19a0be945065177215fcc0d5249e24bd42
Mate Erdelyi <mate.erdelyi@icloud.com>
2021-02-06 10:47:26 +0100

    MZ-03

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,29 @@
 function newTask() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("newTask").value;
-    var task = document.createTextNode(inputValue);
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("newTask").value;
+    let task = document.createTextNode(inputValue);
     li.appendChild(task);
     document.getElementById("tasksList").appendChild(li);
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode(" \u00D7");
+    span.className = "clear";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
 
+let close = document.getElementsByClassName("clear");
+let i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
